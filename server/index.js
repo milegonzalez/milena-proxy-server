39354fc38643d3480f47397046bad3288c37fec0
milegonzalez <milegonzalez@gmail.com>
2019-01-25 15:40:42 -0500

    proxy server now loading

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const proxy = require('express-http-proxy');
+// const proxy = require('express-http-proxy');
 const app = require('express')();
-const port = process.env.PORT || 1120;
+const port = process.env.PORT || 3000;
+const proxy = require('http-proxy-middleware');
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../src/client')));
 
+app.use(proxy('/api/videos', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
+app.use(proxy('/api/games', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
+app.use(proxy('/api/users', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
+
 
 app.listen(port, () => {
   console.log(`server running. Using port ${port}`);
 })
+
